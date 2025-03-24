8d35ef8d9151cce6fd511cde4eb155ea8acb1125
NktBlgv <99440944+BlogPlayCode@users.noreply.github.com>
2025-03-24 13:50:26 +0300

    Update temperatureconverter.c

--- temperatureconverter.c
+++ temperatureconverter.c
@@ -4,7 +4,9 @@ int main() {
     float celsius;
     printf("Введите температуру в Цельсиях: ");
     scanf("%f", &celsius);
+    
     float farengeit = (float) (celsius * 9.0 / 5.0) + 32.0;
+    
     printf("C = %.0f ; F = %.2f\n", celsius, farengeit);
     return 0;
 }
