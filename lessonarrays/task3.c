f9661cca89b248a4fb6716cedae53fc03f76f930
NktBlgv <99440944+BlogPlayCode@users.noreply.github.com>
2025-04-04 11:06:51 +0300

    Update task3.c

--- lessonarrays/task3.c
+++ lessonarrays/task3.c
@@ -2,13 +2,13 @@
 
 int main() {
     const int LENGTH = 3;
-    int colors[LENGTH];
+    int numbers[LENGTH];
 
     for (int i=0; i<LENGTH; i++)
-        colors[i] = i+1;
+        numbers[i] = i+1;
 
     for (int i=0; i<LENGTH; i++)
-        printf("%d\n", colors[i]);
+        printf("%d\n", numbers[i]);
 
     return 0;
 }
